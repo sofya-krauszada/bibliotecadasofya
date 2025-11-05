7f70a945f4c8a5908136790d01361ae5a0187bd2
SOFYA KRAUS COELHO <sofya_coelho@estudante.sesisenai.org.br>
2025-11-05 17:03:09 -0300

    enviado back

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+ import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+// Rota de teste
+app.get("/", (req, res) => {
+ res.json({ message: "API Biblioteca funcionando " });
+});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
